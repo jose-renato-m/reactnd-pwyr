ff483ddeb33ef2d7d361551e9253e960304001c5
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-12-17 20:11:41 -0300

    refactor: Update avatar url

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -2,7 +2,7 @@ let users = {
     sarahedo: {
       id: 'sarahedo',
       name: 'Sarah Edo',
-      avatarURL: ,
+      avatarURL: 'https://tylermcginnis.com/would-you-rather/sarah.jpg'
       answers: {
         "8xf0y6ziyjabvozdd253nd": 'optionOne',
         "6ni6ok3ym7mf1p33lnez": 'optionTwo',
@@ -14,17 +14,17 @@ let users = {
     tylermcginnis: {
       id: 'tylermcginnis',
       name: 'Tyler McGinnis',
-      avatarURL: ,
+      avatarURL: 'https://tylermcginnis.com/would-you-rather/tyler.jpg'
       answers: {
         "vthrdm985a262al8qx3do": 'optionOne',
         "xj352vofupe1dqz9emx13r": 'optionTwo',
       },
       questions: ['loxhs1bqm25b708cmbf3g', 'vthrdm985a262al8qx3do'],
     },
-    johndoe: {
-      id: 'johndoe',
-      name: 'John Doe',
-      avatarURL: ,
+    danabramov: {
+      id: 'danabramov',
+      name: 'Dan Abramov',
+      avatarURL: 'https://tylermcginnis.com/would-you-rather/dan.jpg'
       answers: {
         "xj352vofupe1dqz9emx13r": 'optionOne',
         "vthrdm985a262al8qx3do": 'optionTwo',
