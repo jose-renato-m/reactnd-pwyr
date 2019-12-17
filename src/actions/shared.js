fc1ba9fd95793ab0d5ca2cfd6b1c3c0b4e9f1a0f
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-12-17 19:10:58 -0300

    feat: Export function thus returning objects

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,1 +1,10 @@
 import { getInitialData } from '../utils/api'
+
+export function handleInitialData () {
+    return (dispatch) => {
+        return getInitialData()
+            .then(({ users, questions }) => {
+
+            })
+    }
+}
