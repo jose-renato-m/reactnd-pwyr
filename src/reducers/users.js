4a5b73130a95c3c7323f41a84730ef71f0f0e056
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-12-17 21:07:37 -0300

    style: Delete space

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -2,7 +2,7 @@ import { LOAD_USERS } from '../actions/users'
 
 export default function users (state = {}, action) {
     switch(action.type) {
-        case LOAD_USERS :
+        case LOAD_USERS:
             return {
                 ...state,
                 ...action.users
