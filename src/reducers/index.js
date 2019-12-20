778a120a611026b46521f9a3510ce9b5c34ec698
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-12-19 21:16:02 -0300

    feat: Export the default sum of reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,3 +2,9 @@ import { combineReducers } from 'redux'
 import authedUser from './authedUser'
 import users from './users'
 import questions from './questions'
+
+export default combineReducers({
+    authedUser,
+    users,
+    questions,
+})
