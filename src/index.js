a8b8514bc566eddea242dab3f915b928aebaf947
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-12-21 19:08:38 -0300

    feat: Import sum of reducers

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import './index.css'
 import App from './components/App'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import reducer from './reducers'
 
 ReactDOM.render(<App />, document.getElementById('root'))
