c4c7f408f7641ac932987692aae16443581ab3af
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-12-21 19:03:45 -0300

    feat: Import Provider from library

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
 import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 ReactDOM.render(<App />, document.getElementById('root'))
