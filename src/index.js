7adf4dcc4b44b2d642f3401b4d90f960de300c7d
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-12-21 19:11:23 -0300

    feat: Create variable

--- src/index.js
+++ src/index.js
@@ -6,4 +6,6 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 
+const store = createStore(reducer)
+
 ReactDOM.render(<App />, document.getElementById('root'))
