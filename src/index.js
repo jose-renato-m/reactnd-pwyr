6c8146b6251c0fc37d1a32c662a0b5b8d23ad53b
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-12-21 19:01:33 -0300

    feat: Import createStore from library

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
+import { createStore } from 'redux'
 
 ReactDOM.render(<App />, document.getElementById('root'))
