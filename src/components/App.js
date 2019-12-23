6f4326db94eb9e44cd75f8e1d1ae53d8bd377b23
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-12-23 16:11:06 -0300

    feat: Import Grid from library

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Grid } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
 
