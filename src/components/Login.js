721e6d6423c2ee974ff288dc8a542e9e983ad626
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-12-23 16:37:36 -0300

    feat: Import instances from semantic-ui-react library

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import {
+    Segment,
+    Grid,
+    Header,
+    Image,
+    Form,
+    Loader,
+    Dimmer,
+} from 'semantic-ui-react'
 
 export default Login
