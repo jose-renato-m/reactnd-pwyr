9996c45c02cb97796644173b98be7d648f39f2d4
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-12-24 16:49:23 -0300

    feat: Add component

--- src/components/App.js
+++ src/components/App.js
@@ -18,7 +18,7 @@ class App extends Component {
             <Route
               render={() => (
                 <ContentGrid>
-                  
+                  <Login />
                 </ContentGrid>
               )}
           ) : (
