86118482d71579e1896c011240ef5be32492c8ec
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-12-24 17:07:34 -0300

    feat: Import instances from semantic ui react

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,13 @@
 import React, { Component, Fragment } from 'react'
 import { NavLink } from 'react-router-dom'
 import { connect } from 'react-redux'
+import {
+    Menu,
+    Responsive,
+    Image,
+    Grid,
+    Button,
+    Container,
+} from 'semantic-ui-react'
 
 export default connect()(Nav)
