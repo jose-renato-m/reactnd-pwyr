92f3ed171d34871bcc9da246e4a4e71f81dcd484
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-12-26 18:17:08 -0300

    feat: Export class

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,4 +2,8 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
+export class Home extends Component {
+    
+}
+
 export default connect()(Home)
