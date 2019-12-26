7d9dacbcf544ed175ae7cf5236dad0c63c1709b2
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-12-26 18:50:58 -0300

    feat: Define propTypes arguments

--- src/components/PollTeaser.js
+++ src/components/PollTeaser.js
@@ -3,7 +3,10 @@ import PropTypes from 'prop-types'
 import { Redirect } from 'react-router-dom'
 
 export class PollTeaser extends Component {
-    
+    static propTypes = {
+        question: PropTypes.object.isRequired,
+        unanswered: PropTypes.bool.isRequired,
+    }
 }
 
 export default PollTeaser
