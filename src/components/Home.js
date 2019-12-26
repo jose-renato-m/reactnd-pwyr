a0d707691e1734c301c6b77ac02dff97edbe67fb
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-12-26 18:19:06 -0300

    feat: Declare propTypes

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
 export class Home extends Component {
-    
+    static propTypes = {
+        userQuestionData: PropTypes.object.isRequired
+    }
 }
 
 export default connect()(Home)
