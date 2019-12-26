0a99bef09be9910b21e54c8eadc8b3c705d8c7d4
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-12-26 18:48:01 -0300

    feat: Export class

--- src/components/PollTeaser.js
+++ src/components/PollTeaser.js
@@ -2,4 +2,8 @@ import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { Redirect } from 'react-router-dom'
 
+export class PollTeaser extends Component {
+    
+}
+
 export default PollTeaser
