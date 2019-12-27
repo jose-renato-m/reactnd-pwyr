5abee876461f7241a6976ab92c2416f4be77ac3a
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-12-27 17:32:11 -0300

    feat: Add propTypes parameter

--- src/components/PollQuestion.js
+++ src/components/PollQuestion.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
 export class PollQuestion extends Component {
-    
+    static propTypes = {
+        authedUser: PropTypes.string.isRequired        
+    }
 }
 
 export default connect()(PollQuestion)
