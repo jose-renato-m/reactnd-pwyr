bb882e6fec4c2238170f29dc3232aeb627139f2d
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-12-27 17:03:04 -0300

    feat: Export variables

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -2,6 +2,8 @@ import { saveQuestion } from '../utils/api'
 import { addQuestionToUser } from './users'
 
 export const LOAD_QUESTIONS = 'LOAD_QUESTIONS'
+export const ADD_ANSWER_TO_QUESTION = 'ADD_ANSWER_TO_QUESTION'
+export const ADD_QUESTION = 'ADD_QUESTION'
 
 export function loadQuestions (questions) {
     return {
