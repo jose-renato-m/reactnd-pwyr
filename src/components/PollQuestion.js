1cab4a3ba727522703ec0b07efebde88deda3095
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-12-27 17:29:31 -0300

    feat: Export class component

--- src/components/PollQuestion.js
+++ src/components/PollQuestion.js
@@ -2,4 +2,8 @@ import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
+export class PollQuestion extends Component {
+    
+}
+
 export default connect()(PollQuestion)
