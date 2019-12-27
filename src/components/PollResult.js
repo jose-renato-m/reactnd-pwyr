fbdaec1128b98b4b0c0d2fb678247c5bf43a02fe
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-12-27 14:57:10 -0300

    feat: Instance elements from semantic-ui-react

--- src/components/PollResult.js
+++ src/components/PollResult.js
@@ -2,6 +2,13 @@ import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
-import { Form } from 'semantic-ui-react'
+import {
+    Header,
+    Segment,
+    Progress,
+    Label,
+    Button,
+    Icon,
+} from 'semantic-ui-react'
 
 export default withRouter(connect()(PollResult))
