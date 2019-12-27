649f9099cb0da5c5def479b195070fc888d3074a
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-12-27 16:48:10 -0300

    feat: Add return method

--- src/actions/users.js
+++ src/actions/users.js
@@ -26,6 +26,8 @@ export function handleSaveQuestionAnswer(authedUser, qid, answer) {
         dispatch(addAnswerToUser(authedUser, qid, answer))
         dispatch(addAnswerToQuestion(authedUser, qid, answer))
 
-        
+        return ().catch() => {
+            
+        }
     }
 }
