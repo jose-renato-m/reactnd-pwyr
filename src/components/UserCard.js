5b0a1b659ca0de3dacf2a3710a621b27a9770621
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-12-29 20:36:09 -0300

    feat: Add return method

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -30,6 +30,10 @@ export class UserCard extends Component {
 
         const tabColor = unanswered === true ? colors.green : colors.blue
         const borderTop = unanswered === null ? `1px solid ${colors.grey}` : `2px solid ${tabColor.hex}`
+
+        return (
+            
+        )
     }
 }
 
