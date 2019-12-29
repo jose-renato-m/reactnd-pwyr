1676e2bb9368fa8f9243507af064cb0848023535
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-12-29 20:11:23 -0300

    feat: Import components

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import PollQuestion from './PollQuestion'
+import PollResult from './PollResult'
+import PollTeaser from './PollTeaser'
 
 export default connect()(UserCard)
