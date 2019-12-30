c48c7cf6fa0d6db6a3c383879da2ed5d21a96167
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-12-30 17:43:52 -0300

    feat: Set proptypes

--- src/components/NewPoll.js
+++ src/components/NewPoll.js
@@ -4,7 +4,10 @@ import { connect } from 'react-redux'
 import { handleSaveQuestion } from '../actions/questions'
 
 export class NewPoll extends Component {
-
+    static propTypes = {
+        authedUser: PropTypes.string.isRequired,
+        handleSaveQuestion: PropTypes.func.isRequired,
+    }
 }
 
 export default connect()(NewPoll)
