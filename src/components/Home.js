2b23c0d0459ba6d23411cfa626ad2e5597ebded0
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-12-30 16:56:52 -0300

    feat: Add render method

--- src/components/Home.js
+++ src/components/Home.js
@@ -31,6 +31,12 @@ const panes = props => {
                     ))}
                 </Tab.Pane>
             )
+        },
+        {
+            menuItem: 'Answered',
+            render: () => (
+                
+            )
         }
     ]
 }
