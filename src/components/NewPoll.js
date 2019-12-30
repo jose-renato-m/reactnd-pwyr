47853fb4f93e3a66f4148d089a427ece341222d9
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-12-30 17:47:59 -0300

    feat: Instance elements from semantic ui react

--- src/components/NewPoll.js
+++ src/components/NewPoll.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import {
+    Segment,
+    Header,
+    Grid,
+    Divider,
+    Form,
+    Dimmer,
+    Loader,
+} from 'semantic-ui-react'
 import { handleSaveQuestion } from '../actions/questions'
 
 export class NewPoll extends Component {
     static propTypes = {
         authedUser: PropTypes.string.isRequired,
         handleSaveQuestion: PropTypes.func.isRequired,
-    }
+    }    
 }
 
 export default connect()(NewPoll)
