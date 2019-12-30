2f61d3f285eaf487359dc16daa7f1b9c1e9ed8ef
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-12-30 17:41:16 -0300

    feat: Export created class

--- src/components/NewPoll.js
+++ src/components/NewPoll.js
@@ -3,4 +3,8 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { handleSaveQuestion } from '../actions/questions'
 
+export class NewPoll extends Component {
+
+}
+
 export default connect()(NewPoll)
