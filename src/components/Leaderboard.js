c625f17d5c088a2fbb8e362cc9f0bb876c769333
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-12-31 12:32:39 -0300

    feat: Add proptype initial sets

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -3,7 +3,9 @@ import PropType from 'prop-types'
 import { connect } from 'react-redux'
 
 export class Leaderboard extends Component {
-    
+    static propType = {
+        leaderboardData: PropType.array.isRequired
+    }
 }
 
 export default connect()(Leaderboard)
