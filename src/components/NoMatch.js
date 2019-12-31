1687f6f6c66e2814b916676e645499a952c2c383
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-12-31 15:39:39 -0300

    feat: Add render method

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react'
 import { Container, Header } from 'semantic-ui-react'
 
 export class NoMatch extends Component {
-
+    render() {
+        return (
+            
+        )
+    }
 }
 
 export default NoMatch
