64881831450d2ec2400f20da0e067a226c721020
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-12-31 12:28:38 -0300

    feat: Export class of component

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -2,4 +2,8 @@ import React, { Component, Fragment } from 'react'
 import PropType from 'prop-types'
 import { connect } from 'react-redux'
 
+export class Leaderboard extends Component {
+    
+}
+
 export default connect()(Leaderboard)
