e1784576f27e8346b675aa04400b3fbb12ca8544
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-12-31 15:44:34 -0300

    feat: Add paragraph

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -6,6 +6,7 @@ export class NoMatch extends Component {
         return (
             <Container textAlign='center'>
                 <Header as='h3'>No Match 404 Error</Header>
+                <p>Nothing to see here. Please use the menu to try again.</p>
             </Container>
         )
     }
