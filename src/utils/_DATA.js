4687777c4298ca9cd8c9de3d98514576fb6f158a
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-01-07 19:18:01 -0300

    refactor: Update avatars URL

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -2,7 +2,7 @@ let users = {
     sarahedo: {
       id: 'sarahedo',
       name: 'Sarah Edo',
-      avatarURL: 'https://tylermcginnis.com/would-you-rather/sarah.jpg',
+      avatarURL: 'https://semantic-ui.com/images/avatar2/large/molly.png',
       answers: {
         "8xf0y6ziyjabvozdd253nd": 'optionOne',
         "6ni6ok3ym7mf1p33lnez": 'optionTwo',
@@ -14,17 +14,17 @@ let users = {
     tylermcginnis: {
       id: 'tylermcginnis',
       name: 'Tyler McGinnis',
-      avatarURL: 'https://tylermcginnis.com/would-you-rather/tyler.jpg',
+      avatarURL: 'https://semantic-ui.com/images/avatar2/large/matthew.png',
       answers: {
         "vthrdm985a262al8qx3do": 'optionOne',
         "xj352vofupe1dqz9emx13r": 'optionTwo',
       },
       questions: ['loxhs1bqm25b708cmbf3g', 'vthrdm985a262al8qx3do'],
     },
-    danabramov: {
-      id: 'danabramov',
-      name: 'Dan Abramov',
-      avatarURL: 'https://tylermcginnis.com/would-you-rather/dan.jpg',
+    johndoe: {
+      id: 'johndoe',
+      name: 'John Doe',
+      avatarURL: 'https://semantic-ui.com/images/avatar/large/elliot.jpg',
       answers: {
         "xj352vofupe1dqz9emx13r": 'optionOne',
         "vthrdm985a262al8qx3do": 'optionTwo',
