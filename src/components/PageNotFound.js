46c8c6a7e767b27d3ca5bfbaad83acafb8f691e4
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-01-08 15:14:29 -0300

    feat: Add div content

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -4,7 +4,8 @@ class PageNotFound extends Component {
     render() {
         return (
             <div>
-                
+                <h1>Page Not Found!</h1>
+                <p>Sorry the page could not be found.</p>
             </div>
             <Container textAlign='center'>
                 <Header as='h3'>No Match 404 Error</Header>
