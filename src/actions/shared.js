925bdf6836427d35babccad3e302a104c99d43ea
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-01-07 21:23:15 -0300

    feat: Import loading from library

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,6 +1,7 @@
 import { getInitialData } from '../utils/api'
 import { loadUsers } from './users'
 import { loadQuestions } from './questions'
+import { showLoading, hideLoading } from 'react-redux-loading-bar'
 
 export function handleInitialData () {
     return (dispatch) => {
