5bfa4d52abb148daa7af4eb165ad5a5ef168953a
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-01-08 18:26:45 -0300

    fix: Change return place

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -48,12 +48,11 @@ class Leaderboard extends Component {
             }
             const answeredQuestions = Object.keys(user.answers).length
             const createdQuestions = user.questions.length
-            const score = answeredQuestions + createdQuestions
-
-            return (
-
-            )
+            const score = answeredQuestions + createdQuestions            
         })
+        return (
+            
+        )
     }    
 }
 
