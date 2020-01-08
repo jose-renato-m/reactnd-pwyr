2f732caee2b3695d1384c89bab8e669ccd367df0
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-01-08 18:17:03 -0300

    refactor: Delete uselesses code lines

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -51,56 +51,10 @@ class Leaderboard extends Component {
             const score = answeredQuestions + createdQuestions
 
             return (
-                
+
             )
         })
-    }
-    
-    }
-    render() {
-        const { leaderboardData } = this.props
-
-        return (
-            <Fragment>
-                {leaderboardData.map((user, idx) => (
-                    <Segment.Group key={user.id}>
-                        <Label corner='left' icon='trophy' color={trophyColor[idx]} />
-                        <Grid divided padded>
-                            <Grid.Row>
-                                <Grid.Column width={4} verticalAlign='middle'>
-                                    <Image src={user.avatarURL} />
-                                </Grid.Column>
-                                <Grid.Column width={8}>
-                                    <Header as='h3' textAlign='left'>
-                                        {user.name}
-                                    </Header>
-                                    <Grid>
-                                        <Grid.Column width={12}>Answered questions</Grid.Column>
-                                        <Grid.Column width={4}>{user.answerCount}</Grid.Column>
-                                    </Grid>
-                                    <Divider />
-                                    <Grid>
-                                        <Grid.Column width={12}>Created questions</Grid.Column>
-                                        <Grid.Column width={4}>{user.questionCount}</Grid.Column>
-                                    </Grid>
-                                </Grid.Column>
-                                <Grid.Column width={4} textAlign='center'>
-                                    <Segment.Group>
-                                        <Header as='h5' block attached='top' content='Score' />
-                                        <Segment>
-                                            <Label circular color='green' size='big'>
-                                                {user.questionCount + user.answerCount}
-                                            </Label>
-                                        </Segment>
-                                    </Segment.Group>
-                                </Grid.Column>
-                            </Grid.Row>
-                        </Grid>
-                    </Segment.Group>
-                ))}
-            </Fragment>
-        )
-    }
+    }    
 }
 
 function mapStateToProps({ users }) {
