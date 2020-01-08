8dd7fa881339a4a3dfbea5cf704d40dc41a463ad
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-01-08 15:15:40 -0300

    feat: Delete useless jsx

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -6,11 +6,7 @@ class PageNotFound extends Component {
             <div>
                 <h1>Page Not Found!</h1>
                 <p>Sorry the page could not be found.</p>
-            </div>
-            <Container textAlign='center'>
-                <Header as='h3'>No Match 404 Error</Header>
-                <p>Nothing to see here. Please use the menu to try again.</p>
-            </Container>
+            </div>            
         )
     }
 }
