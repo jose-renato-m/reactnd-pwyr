daefb8c5b9da0e6656d030c8018faddaceacdca3
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-01-08 15:21:02 -0300

    feat: Instance elements from semantic ui react

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { Dropdown, Image, Message } from 'semantic-ui-react'
+import PropTypes from 'prop-types'
+
 import {
     Segment,
     Grid,
