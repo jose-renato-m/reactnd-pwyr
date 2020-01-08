c4fafd5b32efc893c3b214b52bf69ef2fcf170a8
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-01-08 19:54:12 -0300

    feat: Add function

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class QuestionList extends Component {
-    
+    cardItemsPerRow = 2
+
+    getCardsFromQuestions = (filterLogic) => {
+
+    }
 }
 
 export default connect()(QuestionList)
