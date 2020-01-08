ea7a02af0e278ddf43f2c800d5a0af8434b9d9d4
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-01-07 21:30:02 -0300

    refactor: Rework handleInitialData code

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -3,12 +3,11 @@ import { loadUsers } from './users'
 import { loadQuestions } from './questions'
 import { showLoading, hideLoading } from 'react-redux-loading-bar'
 
-export function handleInitialData () {
-    return (dispatch) => {
-        return getInitialData()
-            .then(({ users, questions }) => {
-                dispatch(loadUsers(users))
-                dispatch(loadQuestions(questions))
-            })
-    }
+export const handleInitialData = () => (dispatch) => {
+    dispatch(showLoading())
+    return getInitialData().then(({ users, questions }) => {
+        dispatch(loadUsers(users))
+        dispatch(loadQuestions(questions))
+        dispatch(hideLoading())
+    }) 
 }
