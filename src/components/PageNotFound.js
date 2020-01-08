a1617b51a807faaa15e3f835c38036d83e26b8a2
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-01-08 15:12:27 -0300

    feat: Add div jsx

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react'
 class PageNotFound extends Component {
     render() {
         return (
+            <div>
+                
+            </div>
             <Container textAlign='center'>
                 <Header as='h3'>No Match 404 Error</Header>
                 <p>Nothing to see here. Please use the menu to try again.</p>
