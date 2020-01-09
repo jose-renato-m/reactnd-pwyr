e2a3d3b404e0c303ca0dbd1add25f65398170bff
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-01-08 21:20:46 -0300

    feat: Set initial state

--- src/components/QuestionNew.js
+++ src/components/QuestionNew.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class QuestionNew extends Component {
-
+    state = {
+        optionOne: '',
+        optionTwo: '',
+        message: { hidden: true, content: ''}
+    }
 }
 
 export default connect() (QuestionNew)
