7a621fc2b1356a50a661e53ffb9c4d4976711346
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-01-10 15:09:05 -0300

    feat: Set initial state

--- src/components/QuestionView.js
+++ src/components/QuestionView.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 import { handleAnswerQuestion } from '../actions/questions'
 
 class QuestionView extends Component {
-
+    state = { votedForOption: null, message: { hidden: true, content: '' }}
 }
 
 export default connect()(QuestionView)
