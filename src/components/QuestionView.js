10b498e44d7e5fc2128ec331512f857d433ba89c
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-01-10 15:06:47 -0300

    feat: Prepare component with basic structure

---
+++ src/components/QuestionView.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleAnswerQuestion } from '../actions/questions'
+
+class QuestionView extends Component {
+
+}
+
+export default connect()(QuestionView)
