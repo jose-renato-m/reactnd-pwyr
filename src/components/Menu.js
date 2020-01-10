fc3ce3090b7d422dad9eaeb9e01e3bd7b3e61757
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-01-10 20:39:42 -0300

    feat: Add render method

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class Menu extends Component {
-
+    render() {
+        return (
+            
+        )
+    }
 }
 
 export default connect()(Menu)
