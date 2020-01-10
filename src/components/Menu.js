f77b84a405e40fead43bd22f97a38d1fe95a19b3
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-01-10 20:42:00 -0300

    feat: Destructure objects

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,8 +3,11 @@ import { connect } from 'react-redux'
 
 class Menu extends Component {
     render() {
+        const { users, authedUser } = this.props
+        const { name, avatarURL } = users[authedUser]
+
         return (
-            
+
         )
     }
 }
