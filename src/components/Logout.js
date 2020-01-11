034332cee6e74a5ba099a31c92e0d84098dea17d
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-01-10 21:10:33 -0300

    feat: Add componentdidmount method

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class Logout extends Component {
-
+    componentDidMount() {
+        
+    }
 }
 
 export default connect()(Logout)
