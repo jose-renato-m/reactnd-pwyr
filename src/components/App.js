cdcaffa7fa1f428d8e0ee711137f27d2e9cdc311
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-01-11 17:29:46 -0300

    feat: Add footer element

--- src/components/App.js
+++ src/components/App.js
@@ -81,6 +81,7 @@ class App extends Component {
                    <Route path='/' component={PageNotFound} />
               </Switch>
             </div>
+            <Footer />
           </Fragment>
       </BrowserRouter>         
     )
