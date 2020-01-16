1051d036368188ad4023d2f64c6451ea2a99affd
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-01-16 15:07:28 -0300

    feat: Define proptypes

--- src/components/RequiresAuth.js
+++ src/components/RequiresAuth.js
@@ -2,6 +2,10 @@ import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
 
 class RequiresAuth extends React.Component {
+    static propTypes = {
+        isAuthenticated: PropTypes.boolean,
+        redirect: PropTypes.func.isRequired
+    }
 
 }
 
