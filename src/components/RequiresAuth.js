d74e8950c4068f1509dfc8be2f777fbfce478a00
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-01-16 16:02:50 -0300

    feat: Import bindactioncreators from library

--- src/components/RequiresAuth.js
+++ src/components/RequiresAuth.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { push } from 'react-router-redux'
+import { bindActionCreators } from 'redux'
 
 export default function (ComposedComponent) {
     class RequiresAuth extends React.Component {
