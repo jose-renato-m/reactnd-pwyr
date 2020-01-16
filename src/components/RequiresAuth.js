b82bf92224332fca7ca7e2810b2972d980c4e522
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-01-16 15:31:35 -0300

    feat: Export function encapsulating it

--- src/components/RequiresAuth.js
+++ src/components/RequiresAuth.js
@@ -1,36 +1,38 @@
 import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
 
-class RequiresAuth extends React.Component {
-    static propTypes = {
-        isAuthenticated: PropTypes.boolean,
-        redirect: PropTypes.func.isRequired
-    }
-
-    componentDidMount() {
-        this._checkAndRedirect()
-    }
-
-    componentDidUpdate() {
-        this._checkAndRedirect()
-    }
-
-    _checkAndRedirect() {
-        const { isAuthenticated, redirect } = this.props
-
-        if (!isAuthenticated) {
-            redirect()
+export default function (ComposedComponent) {
+    class RequiresAuth extends React.Component {
+        static propTypes = {
+            isAuthenticated: PropTypes.boolean,
+            redirect: PropTypes.func.isRequired
         }
+    
+        componentDidMount() {
+            this._checkAndRedirect()
+        }
+    
+        componentDidUpdate() {
+            this._checkAndRedirect()
+        }
+    
+        _checkAndRedirect() {
+            const { isAuthenticated, redirect } = this.props
+    
+            if (!isAuthenticated) {
+                redirect()
+            }
+        }
+    
+        render() {
+            return (
+                <div>
+    
+                </div>
+            )
+        }
+    
     }
-
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
-
+    
+    return connect()(RequiresAuth)
 }
-
-return connect()(RequiresAuth)
