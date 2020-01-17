e184a8a1a10fea14475f4bf6060d4ce8a36611ff
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-01-17 14:45:13 -0300

    refactor: Replace render method by component

--- src/components/App.js
+++ src/components/App.js
@@ -42,27 +42,13 @@ class App extends Component {
             {isAuthenticated && <Menu />}
             <div className='ui main text container' style={{ marginTop: '7em' }}>
               <Switch>
-                <Route
-                  path='/'
-                  exact            
-                  component={RequiresAuth(QuestionList)}                 
-                 />
-                 <Route
-                    path='/add'
-                    render={(history) => {
-                      return (
-                        <QuestionNew
-                          resetActiveIndexToZero={this.resetActiveIndexToZero}
-                          history={history.history}
-                         />
-                      )
-                    }}
-                   />
-                   <Route path='/questions/:question_id' component={QuestionView} />
-                   <Route path='/leaderboard' component={Leaderboard} />                                     
-                   <Route path='/logout' component={Logout} />                   
-                   <Route path='/404' component={PageNotFound} />    
-                   <Route path='/' component={QuestionList} />            
+                <Route path='/' exact component={RequiresAuth(QuestionList)} />
+                <Route path='/add' component={QuestionNew} />
+                <Route path='/questions/:question_id' component={QuestionView} />
+                <Route path='/leaderboard' component={Leaderboard} />                                     
+                <Route path='/logout' component={Logout} />                   
+                <Route path='/404' component={PageNotFound} />    
+                <Route path='/' component={QuestionList} />            
               </Switch>
             </div>
             <Footer />
