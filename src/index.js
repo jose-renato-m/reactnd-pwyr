69a16f39691a297c50c194f2ec599664fcb6e986
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-01-21 16:40:13 -0300

    refactor: Replace from history to record

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import 'semantic-ui-css/semantic.min.css'
 import { createBrowserHistory } from 'history'
 import { connectRouter, routerMiddleware, ConnectedRouter } from 'connected-react-router'
 import App from './components/App'
-import history from './history'
+import record from './record'
 
 const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose
 
@@ -22,7 +22,7 @@ const store = createStore(
 
 ReactDOM.render(
     <Provider store={store}>
-        <ConnectedRouter history={history}>
+        <ConnectedRouter record={record}>
             <App />
         </ConnectedRouter>                 
     </Provider>, 
