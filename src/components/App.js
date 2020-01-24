35caabc325e78dd71a0e24cb6eddfbf9c5f5efff
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-01-24 11:30:44 -0300

    refactor: Add authentication to question view

--- src/components/App.js
+++ src/components/App.js
@@ -40,7 +40,7 @@ class App extends Component {
                 <Route path='/' exact component={RequiresAuth(QuestionList)} />
                 <Route path='/add' component={RequiresAuth(QuestionNew)} />
                 <Route path='/login' component={Login} />                
-                <Route path='/questions/:question_id' component={QuestionView} />
+                <Route path='/questions/:question_id' component={RequiresAuth(QuestionView)} />
                 <Route path='/leaderboard' component={RequiresAuth(Leaderboard)} />                                                     
                 <Route path='/logout' component={Logout} />                
                 <Route path='/404' component={PageNotFound} />                          
